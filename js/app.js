a037448f75c1503b0dc52fc78abd8a1708849ef2
Christopher Acosta <chrisacosta12345@gmail.com>
2023-10-25 14:35:19 -0700

    added more prompts

--- js/app.js
+++ js/app.js
@@ -1,45 +1,52 @@
 'use strict';
 
+let rightAnswers = 0;
+
 let user = prompt('What is your name?');
 
-let birthPlace = prompt('Yes or No: Chris was born in Arizona');
+let birthPlace = prompt('Chris was born in Arizona (Y/N)');
 
 let normBirthPlace = birthPlace.toLowerCase();
 
-if (normBirthPlace=== 'yes' || normBirthPlace === 'y') {
+if (normBirthPlace.startsWith("y")) {
   //console.log('nope');
   alert('Nope! Check my About Me for the answer!');
-} else if (normBirthPlace=== 'no' || normBirthPlace=== 'n'){
+} else if (normBirthPlace.startsWith("n")){
   //console.log('yup');
-  alert('Correct!, I was born in California!');
+  alert('Correct!, I was born in California!')
+  rightAnswers++;
 } else {
   //console.log('nah');
   alert('Please type yes or no');
 }
+console.log(rightAnswers);
 
-let service = prompt('Yes or No: Chris served in the Marine Corps');
+let service = prompt('Chris served in the Marine Corps (Y/N)');
 
 let normService = service.toLowerCase();
 
-if (normService === 'yes' || normService === 'y'){
+if (normService.startsWith("y")){
   //console.log('negative');
   alert('Negative, Chris is no crayon muncher. Check the About Me for the right answer!');
-} else if (normService === 'no' || normService === 'n'){
+} else if (normService.startsWith("n")){
   //console.log('Correct!');
   alert('Roger Roger, I was in the Army');
+  rightAnswers++;
 } else {
   //console.log('no');
   alert('Please type yes or no');
 }
+console.log(rightAnswers);
 
-let work = prompt('Yes or No: Chris worked in Washington as a Habitat Conservation worker');
+let work = prompt('Chris worked in Washington as a Habitat Conservation worker (Y/N)');
 
 let normWork = work.toLowerCase();
 
-if (normWork=== 'yes' || normWork === 'y'){
+if (normWork.startsWith("y")){
   //console.log('correct');
   alert("That's right!");
-} else if (normWork=== 'no' || normWork=== 'n'){
+  rightAnswers++;
+} else if (normWork.startsWith("n")){
   //console.log('wrong');
   alert('WRONG!');
 } else {
@@ -47,37 +54,84 @@ if (normWork=== 'yes' || normWork === 'y'){
   alert('Please type yes or no');
 }
 
-let pet = prompt('Yes or No: Chris has a dog named Champ');
+let pet = prompt('Chris has a dog named Champ Y/N');
 
 let normPet = pet.toLowerCase();
 
-if (normPet === 'yes' || normPet === 'y'){
+if (normPet.startsWith("y")){
   //console.log('correct');
   alert('That is correct! Champ is happy!');
-} else if (normPet === 'no' || normPet === 'n'){
+  rightAnswers++;
+} else if (normPet.startsWith("n")){
   //console.log('wrong');
   alert('That is wrong, Champ is sad. Thanks...');
 } else {
   //console.log('NO');
   alert('Please type yes or no');
 }
+console.log(rightAnswers);
 
-let home = prompt('Yes or NO: Chris currently lives in California');
+let home = prompt('Chris currently lives in California (Y/N)');
 
 let normHome = home.toLowerCase();
 
-if (normHome=== 'yes' || normHome === 'y'){
+if (normHome.startsWith("y")){
   //console.log('correct');
   alert("That's right!");
-} else if (normHome=== 'no' || normHome=== 'n'){
+  rightAnswers++
+
+} else if (normHome.startsWith("n")){
   //console.log('wrong');
   alert('WRONG!');
 } else {
   //console.log('NO');
   alert('Please type yes or no');
 }
+console.log(rightAnswers);
+
 
 alert(`Welcome to my site ${user}!`);
 
+for (let i = 0; i < 5; i++) {
+  let guess = prompt("How old am I?");
+  guess = Number(guess);
+  if (guess === 30){
+    alert("Right!");
+    rightAnswers++;
+    break;
+  }
+  else if (guess !== 30 && i < 4){
+    alert("Incorrect, please try again");
+  }
+  if (i === 3){
+    alert("The correct answer is 30!");
+    break;
+  }
+}
+console.log(rightAnswers);
 
+let possibleColors = ["green","blue", "red"];
 
+let attempts = 6;
+
+while (attempts > 0) {
+  let guess = prompt("Guess one of my favorite colors!").toLowerCase();
+
+  if (possibleColors.includes(guess)){
+    alert(`Correct! ${guess} is an awesome color right?`);
+    rightAnswers++;
+    break;
+  } else {
+    attempts--;
+    if(attempts > 0) {
+      alert(`Wrong, you have ${attempts} remaining.`)
+    } else {
+      alert(`Sorry, you are out of attempts, here are the right answers: ${possibleColors}`);
+    }
+  }
+}
+console.log(rightAnswers);
+alert(rightAnswers);
+
+//ChatGPT helped me write this code!
+//
